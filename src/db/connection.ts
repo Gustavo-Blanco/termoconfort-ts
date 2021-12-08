eed621742fe8d1d534ce9df3b76fbfc59cda8d35
Gustavo Blanco Ticona <gustavo.blanco@tecsup.edu.pe>
2021-12-07 22:55:31 -0500

    logs off

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,7 +3,7 @@ import env from "../config/env";
 
 mongoose
   .connect(env.DB_CONNECTION_URL)
-  .then((db) => db.set("debug", true))
+  // .then((db) => db.set("debug", true))
   .then((db) => console.log("Connected to database on port " + env.DB_HOST_PORT))
   .catch((error) => console.log("Error database " + error));
 
